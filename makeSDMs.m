6544b7856571421319341db15180a1e5dd48ef21
Jessica Thompson <j.thompson@umontreal.ca>
2016-01-05 13:56:29 -0500

    fixed indexing error for 5D features

--- makeSDMs.m
+++ makeSDMs.m
@@ -1,4 +1,4 @@
-function makeSDMs(HRF, minus, dim, scaled, MC, subs)
+function makeSDMs(HRF, minus, dim, scaled, MC, subs, feat)
 % makeSDMs - Make various design matrix files for running GLMs in BVQX
 % Saves design matrices describing ripple stimuli in fMRI experiment
 %
@@ -6,19 +6,24 @@ function makeSDMs(HRF, minus, dim, scaled, MC, subs)
 %
 % Inputs:
 %    HRF - 1 to apply canonical hrf. 2 to deconvlve. 0 none
-%    minus - 1 to use minus predictors or no (e.g. f01-f02), 0 for just f01
-%    dim - Current options: 4 or 13 (could add 3 maybe 6)
+%    minus - 1 to use minus predictors (e.g. f01-f02), 0 for just f01
+%    dim - Current options: 4, 5, 13, 15 (could add 3 maybe 6)
+%          if 5: separate sdms will be made for each f0, Rt, Om
 %    scaled - 1 if scaledd (default), 0 if flat
 %    MC - 1 if add motion correction preds, 0 otherwise (default)
 %    subs - Optional. Cell array of subject identifiers. Default all subs.
+%    feat - If dim=5, feat specifies which feature to save: 1 (f0), 2(Rt),
+%           3(Om)
 %
 % Outputs:
 %    Nothing returned, saves .sdm files
 %
 % Example: 
 %    makeSDMs(2, 1, 4, 1, 0, {'s01'})
-%    makeSDMs(2, 1, 4, 1, 0, {'s01','s02', 's03', 's04', 's05', 's05'})
-%    makeSDMs(1, 1, 4, 1, 1, {'s01','s02', 's03', 's04', 's05', 's05'})
+%    makeSDMs(2, 1, 4, 1, 0, {'s01','s02', 's03', 's04', 's05', 's06'})
+%    makeSDMs(1, 1, 4, 1, 1, {'s01','s02', 's03', 's04', 's05', 's06'})
+%    makeSDMs(1, 0, 15, 1, 0, {'s02'})
+%    makeSDMs(1, 0, 5, 1, 0, {'s02'}, 2)
 %
 % Other m-files required: none
 % Subfunctions: none
@@ -33,6 +38,9 @@ function makeSDMs(HRF, minus, dim, scaled, MC, subs)
 %------------- BEGIN CODE --------------
 
 % Process input arguments
+if nargin < 7 && dim == 5
+    feat = 1;
+end
 if nargin < 6
     subs = {'s01', 's02', 's03', 's04', 's05', 's06'};
 end
@@ -48,10 +56,11 @@ os = getenv('OS');
 if strfind(os, 'Windows')
     datadir = 'D:\ripples\';
 else 
-    datadir = '/Users/jthompson/data/ripples/';
+    datadir = '/Users/jthompson/Dropbox/ripples/';
 end
 MC_pred_names = {'Trans-X', 'Trans-Y', 'Trans-Z', ...
                     'Rot-X', 'Rot-Y', 'Rot-Z'};
+feats = {'f0', 'Rt', 'Om'};
 for s=1:length(subs)
     sub = subs{s};
     display(sub);
@@ -75,6 +84,8 @@ for s=1:length(subs)
     sdmdir = [subjdir  'sdms' filesep];
 
     % Load ripple indicator features (3D) in variable 'features'
+    % Order of features is f01, f02, Rt1, Rt2, Om1, Om2
+    % Feature values indicate relative presence from 0 to 1
     load([sounddir 'ripple_features_6d.mat']);
 
     % Load order of sound Decomp
@@ -149,7 +160,56 @@ for s=1:length(subs)
                         sdm.SDMMatrix = (sdm.SDMMatrix>0)*1;
                     end
                 end
-                
+            case 5
+                sdm.PredictorColors = repmat(colors, 5, 1);
+                %sdm.PredictorColors = cat(1, sdm.PredictorColors, black);
+                if minus
+                    fname = [fname '_minus'];
+                    sdm.PredictorNames = {'f01-f01=-1', 'Rt1-Rt2=-1', ...
+                        'Om1-Om2=-1', ...
+                        'f01-f01=-33', 'Rt1-Rt2=-33', 'Om1-Om2=-33', ...
+                        'f01-f01=0', 'Rt1-Rt2=0', 'Om1-Om2=0', ...
+                        'f01-f01=33', 'Rt1-Rt2=33','Om1-Om2=33', ...
+                        'f01-f01=1', 'Rt1-Rt2=1', 'Om1-Om2=1'};
+%                     RI4D_minus(:,1) = RI_6D(:,1)-RI_6D(:,2);
+%                     RI4D_minus(:,2) = RI_6D(:,3)-RI_6D(:,4);
+%                     RI4D_minus(:,3) = RI_6D(:,5)-RI_6D(:,6);
+%                     SoundOn = RI_6D(:,1)+RI_6D(:,2);
+%                     sdmMatm1 = RI4D_minus==-1;
+%                     sdmMatm33 = RI4D_minus==-1/3;
+%                     sdmMat33 = RI4D_minus==1/3;
+%                     sdmMat1 = RI4D_minus==1;
+% 
+%                     % Concatenate matrices to give 4*3 = 12 predictors
+%                     sdm.SDMMatrix = cat(2, sdmMatm1, sdmMatm33, ...
+%                         sdmMat33, sdmMat1, SoundOn);
+                else
+                    sdm.PredictorNames = {'f01=0', 'Rt1=0', 'Om1=0', ...
+                        'f01=33', 'Rt1=33', 'Om1=33', ...
+                        'f01=05', 'Rt1=05', 'Om1=05', ...
+                        'f01=66', 'Rt1=66', 'Om1=66', ...
+                        'f01=1', 'Rt1=1', 'Om1=1'};
+                    % f01, Rt1 and Om1 are 0 when f02, Rt2 and Om2 are 1
+                    sdmMat00 = RI_6D(:,[2,4,6])==1; 
+                    sdmMat33 = RI_6D(:,[1,3,5])==1/3;
+                    sdmMat05 = RI_6D(:,[1,3,5])==1/2;
+                    sdmMat66 = RI_6D(:,[1,3,5])==2/3;
+                    sdmMat1 = RI_6D(:,[1,3,5])==1;
+                    sdm.SDMMatrix = cat(2, sdmMat00, sdmMat33, ...
+                        sdmMat05, sdmMat66, sdmMat1)*1; % to make doubles
+                end
+                % Reorder to help with visual inspection
+                % f01(0 to 1), Rt1 (0 to 1), Om (0 to 1)
+                reorder = [1 4 7 10 13 2 5 8 11 14 3 6 9 12 15];
+                sdm.SDMMatrix = sdm.SDMMatrix(:,reorder);
+                sdm.PredictorNames = sdm.PredictorNames(reorder);
+                sdm.PredictorColors = sdm.PredictorColors(reorder,:);
+                % take only 5 predictors for specified feature
+                featidx = 1+(5*(feat-1));
+                fname = [fname '_' feats{feat}];
+                sdm.SDMMatrix = sdm.SDMMatrix(:,featidx:featidx+4);
+                sdm.PredictorNames = sdm.PredictorNames(featidx:featidx+4);
+                sdm.PredictorColors = sdm.PredictorColors(featidx:featidx+4, :);
             case 13
                 sdm.PredictorColors = repmat(colors, 4, 1);
                 sdm.PredictorColors = cat(1, sdm.PredictorColors, black);
@@ -180,11 +240,56 @@ for s=1:length(subs)
                     sdmMat05 = RI_6D(:,[1,3,5])==1/2;
                     sdmMat66 = RI_6D(:,[1,3,5])==2/3;
                     sdmMat1 = RI_6D(:,[1,3,5])==1;
+                    SoundOn = RI_6D(:,1)+RI_6D(:,2);
                     sdm.SDMMatrix = cat(2, sdmMat33, sdmMat05, sdmMat66, ...
-                        sdmMat1);
+                        sdmMat1, SoundOn);
                 end
                 % Reorder to help with visual inspection
                 reorder = [1 4 7 10 2 5 8 11 3 6 9 12 13];
+                sdm.SDMMatrix = sdm.SDMMatrix(:,reorder);
+                sdm.PredictorNames = sdm.PredictorNames(reorder);
+                sdm.PredictorColors = sdm.PredictorColors(reorder, :);
+            case 15
+                sdm.PredictorColors = repmat(colors, 5, 1);
+                %sdm.PredictorColors = cat(1, sdm.PredictorColors, black);
+                if minus
+                    fname = [fname '_minus'];
+                    sdm.PredictorNames = {'f01-f01=-1', 'Rt1-Rt2=-1', ...
+                        'Om1-Om2=-1', ...
+                        'f01-f01=-33', 'Rt1-Rt2=-33', 'Om1-Om2=-33', ...
+                        'f01-f01=0', 'Rt1-Rt2=0', 'Om1-Om2=0', ...
+                        'f01-f01=33', 'Rt1-Rt2=33','Om1-Om2=33', ...
+                        'f01-f01=1', 'Rt1-Rt2=1', 'Om1-Om2=1'};
+%                     RI4D_minus(:,1) = RI_6D(:,1)-RI_6D(:,2);
+%                     RI4D_minus(:,2) = RI_6D(:,3)-RI_6D(:,4);
+%                     RI4D_minus(:,3) = RI_6D(:,5)-RI_6D(:,6);
+%                     SoundOn = RI_6D(:,1)+RI_6D(:,2);
+%                     sdmMatm1 = RI4D_minus==-1;
+%                     sdmMatm33 = RI4D_minus==-1/3;
+%                     sdmMat33 = RI4D_minus==1/3;
+%                     sdmMat1 = RI4D_minus==1;
+% 
+%                     % Concatenate matrices to give 4*3 = 12 predictors
+%                     sdm.SDMMatrix = cat(2, sdmMatm1, sdmMatm33, ...
+%                         sdmMat33, sdmMat1, SoundOn);
+                else
+                    sdm.PredictorNames = {'f01=0', 'Rt1=0', 'Om1=0', ...
+                        'f01=33', 'Rt1=33', 'Om1=33', ...
+                        'f01=05', 'Rt1=05', 'Om1=05', ...
+                        'f01=66', 'Rt1=66', 'Om1=66', ...
+                        'f01=1', 'Rt1=1', 'Om1=1'};
+                    % f01, Rt1 and Om1 are 0 when f02, Rt2 and Om2 are 1
+                    sdmMat00 = RI_6D(:,[2,4,6])==1; 
+                    sdmMat33 = RI_6D(:,[1,3,5])==1/3;
+                    sdmMat05 = RI_6D(:,[1,3,5])==1/2;
+                    sdmMat66 = RI_6D(:,[1,3,5])==2/3;
+                    sdmMat1 = RI_6D(:,[1,3,5])==1;
+                    sdm.SDMMatrix = cat(2, sdmMat00, sdmMat33, ...
+                        sdmMat05, sdmMat66, sdmMat1)*1; % to make doubles
+                end
+                % Reorder to help with visual inspection
+                % f01(0 to 1), Rt1 (0 to 1), Om (0 to 1)
+                reorder = [1 4 7 13 10 2 5 8 14 11 3 6 9 12 15];
                 sdm.SDMMatrix = sdm.SDMMatrix(:,reorder);
                 sdm.PredictorNames = sdm.PredictorNames(reorder);
                 sdm.PredictorColors = sdm.PredictorColors(reorder, :);
