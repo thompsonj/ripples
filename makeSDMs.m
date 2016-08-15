a59667a50c70e0bf8530ed8472255ecffe0bc827
Jessica Thompson <j.thompson@umontreal.ca>
2016-08-15 16:26:17 +0200

    add additional cases for working with a single ripple parameter

--- makeSDMs.m
+++ makeSDMs.m
@@ -2,21 +2,22 @@ function makeSDMs(HRF, minus, dim, scaled, MC, subs, feat)
 % makeSDMs - Make various design matrix files for running GLMs in BVQX
 % Saves design matrices describing ripple stimuli in fMRI experiment
 %
-% Syntax:  makeSDMs(HRF, minus, dim, scaled, MC, subs)
+% Syntax:  makeSDMs(HRF, minus, dim, scaled, MC, subs, feat)
 %
 % Inputs:
-%    HRF - 1 to apply canonical hrf. 2 to deconvlve. 0 none
+%    HRF - 1 to apply canonical hrf.  0 none
 %    minus - 1 to use minus predictors (e.g. f01-f02), 0 for just f01
-%    dim - Current options: 4, 5, 13, 15 (could add 3 maybe 6)
-%          if 5: separate sdms will be made for each f0, Rt, Om
+%    dim - Current options: 1, 4, 5, 13, 15 (could add 3 maybe 6)
+%          if 5 or 1: separate sdms will be made for each f0, Rt, Om
 %    scaled - 1 if scaledd (default), 0 if flat
 %    MC - 1 if add motion correction preds, 0 otherwise (default)
-%    subs - Optional. Cell array of subject identifiers. Default all subs.
-%    feat - If dim=5, feat specifies which feature to save: 1 (f0), 2(Rt),
-%           3(Om)
+%    subs - Cell array of subject identifiers. Default: all subs.
+%    feat - If dim=5 or 1, feat specifies which feature to save: 1 (f0), 
+%                  2(Rt), 3(Om)
+
 %
 % Outputs:
-%    Nothing returned, saves .sdm files
+%    Nothing returned, saves .sdm files in <subdir>/sdms/
 %
 % Example: 
 %    makeSDMs(2, 1, 4, 1, 0, {'s01'})
@@ -24,6 +25,13 @@ function makeSDMs(HRF, minus, dim, scaled, MC, subs, feat)
 %    makeSDMs(1, 1, 4, 1, 1, {'s01','s02', 's03', 's04', 's05', 's06'})
 %    makeSDMs(1, 0, 15, 1, 0, {'s02'})
 %    makeSDMs(1, 0, 5, 1, 0, {'s02'}, 2)
+%    makeSDMs(1, 0, 5, 1, 0, {'s01','s02', 's03', 's04', 's05', 's06'}, opts)
+
+% opts.feat = 1;
+% opts.Betadir = 'Betas_allconds_2016-01-27';
+%    makeSDMs(0, 0, 3, 1, 0, {'s01','s02', 's03', 's04', 's05', 's06'}, 1)
+%    makeSDMs(0, 0, 2, 1, 0, {'s01','s02', 's03', 's04', 's05', 's06'}, 1)
+
 %
 % Other m-files required: none
 % Subfunctions: none
@@ -38,8 +46,10 @@ function makeSDMs(HRF, minus, dim, scaled, MC, subs, feat)
 %------------- BEGIN CODE --------------
 
 % Process input arguments
-if nargin < 7 && dim == 5
+if nargin < 7
     feat = 1;
+else
+    feat = feat;
 end
 if nargin < 6
     subs = {'s01', 's02', 's03', 's04', 's05', 's06'};
@@ -50,8 +60,11 @@ end
 if nargin < 4
     scaled = 1;
 end
-nstick = 8;
-nfeatures = dim + (HRF==2)*dim*(nstick-1);
+
+% Initalize varibales
+%nstick = 8;
+%nfeatures = dim + (HRF==2)*dim*(nstick-1);
+nfeatures = dim;
 os = getenv('OS');
 if strfind(os, 'Windows')
     datadir = 'D:\ripples\';
@@ -61,6 +74,8 @@ end
 MC_pred_names = {'Trans-X', 'Trans-Y', 'Trans-Z', ...
                     'Rot-X', 'Rot-Y', 'Rot-Z'};
 feats = {'f0', 'Rt', 'Om'};
+
+% Create SDM for each subject and each run
 for s=1:length(subs)
     sub = subs{s};
     display(sub);
@@ -77,11 +92,11 @@ for s=1:length(subs)
         comboruns = [1, 4, 5, 8, 9, 12];
         nruns=12;
     end
-    subjdir = [datadir sub filesep];
+    subdir = [datadir sub filesep];
     sounddir = [datadir 'StimuliEncoding' filesep];
-    logdir = [subjdir  'logs' filesep];
-%     prtdir = [subjdir filesep 'prts' filesep];
-    sdmdir = [subjdir  'sdms' filesep];
+    logdir = [subdir  'logs' filesep];
+%     prtdir = [subdir filesep 'prts' filesep];
+    sdmdir = [subdir  'sdms' filesep];
 
     % Load ripple indicator features (3D) in variable 'features'
     % Order of features is f01, f02, Rt1, Rt2, Om1, Om2
@@ -118,15 +133,47 @@ for s=1:length(subs)
         % Initialize basic 6D features and unchanging properties
         RI_6D = zeros(nvols,6);
         RI_6D(stiminfo_decomp(:,1),:) = features(stiminfo_decomp(:,2),:);
-        npredictors = nfeatures +1 + MC*6 ;
-        sdm.SDMMatrix = zeros(nvols,npredictors);
+        npredictors = nfeatures + 1*(nfeatures~=2) + MC*6; % right now 2 indicates the fuller version of the 1d sdm, not actually 2 features
+        sdm.SDMMatrix = ones(nvols,npredictors);
         sdm.NrOfPredictors = npredictors;
+%         sdm.RFX-GLM = 0; % Fixed effect glm for single subjects
         sdm.IncludesConstant = 1;
-        sdm.FirstConfoundPredictor = nfeatures + 1;
+        sdm.FirstConfoundPredictor = nfeatures + 1*(nfeatures~=2);
         fname = '';
         colors = [[255 175 0]; [0 175 65]; [255 0 65]];
         black = [255 255 255];
+        ft=feats{feat};
         switch dim
+            case 1 % one sdm per with a single predictor
+                
+                sdm.PredictorColors = black;
+                sdm.PredictorNames = ft;
+                vec = zeros(size(RI_6D(:,1)));
+                vec(RI_6D(:,feat*2-1)>1/2) = 1;
+                vec(RI_6D(:,feat*2-1)==1/2) = 2;
+                vec(RI_6D(:,feat*2-1)<1/2) = 3;
+                sdm.SDMMatrix(:,1) = vec;
+                fname = [fname '_' feats{feat}];
+                
+            case 2  
+                sdm.PredictorColors = black;
+                sdm.PredictorNames = ft;
+                vec = zeros(size(RI_6D(:,1)));
+                vec(RI_6D(:,feat*2-1)==1) = 1;
+                vec(RI_6D(:,feat*2-1)==1/3) = 2;
+                vec(RI_6D(:,feat*2-1)==1/2) = 3;
+                vec(RI_6D(:,feat*2-1)==2/3) = 4;
+                vec(RI_6D(:,feat*2)==1) = 5;
+                sdm.SDMMatrix(:,1) = vec;
+                fname = [fname '_' feats{feat}];
+            case 3
+                sdm.PredictorColors = colors;
+                sdm.PredictorNames = {[ft '_1'],[ft '='],[ft '_2']};
+                fname = [fname '_' feats{feat}];
+                sdm.SDMMatrix(:,1) = RI_6D(:,feat*2-1)>1/2;
+                sdm.SDMMatrix(:,2) = RI_6D(:,feat*2)==1/2;
+                sdm.SDMMatrix(:,3) = RI_6D(:,feat*2)>1/2;
+                
             case 4
                 sdm.PredictorColors = cat(1, colors, black);
                 if minus 
@@ -295,20 +342,12 @@ for s=1:length(subs)
                 sdm.PredictorColors = sdm.PredictorColors(reorder, :);
         end
                 
-        switch HRF
-            case 1 % Apply canonical HRF
-                fname = [fname '_cHRF'];
-                sdm.SDMMatrix(:,1:nfeatures) = canonicalHRF(sdm.SDMMatrix(:,1:nfeatures));
-            case 2 % Add stick predictors for deconvolution
-                fname = [fname '_deconv'];
-                [sdm.SDMMatrix(:,1:nfeatures), sdm.PredictorNames] = ...
-                    deconvolveHRF(sdm.SDMMatrix(:,1:nfeatures), ...
-                    sdm.PredictorNames, nstick);
-                % todo: colors
-                
+        if HRF==1
+            fname = [fname '_cHRF'];
+            sdm.SDMMatrix(:,1:nfeatures) = canonicalHRF(sdm.SDMMatrix(:,1:nfeatures));
         end
         % Add constant predictor
-        sdm.SDMMatrix(:,nfeatures + 1) = ones; % Constant
+        sdm.SDMMatrix(:,nfeatures + 1*(nfeatures~=2)) = ones; % Constant
         sdm.PredictorNames = cat(2, sdm.PredictorNames, {'Constant'});
         % Add motion correction predictors
         if MC
@@ -316,22 +355,23 @@ for s=1:length(subs)
             sdm.PredictorNames = cat(2, sdm.PredictorNames, MC_pred_names);
             % Load MC sdm
             if strcmp(sub, 's01')
-                mcfname = [subjdir 'images' filesep ...
+                mcfname = [subdir 'images' filesep ...
                     sprintf('ripples_%s_run%d_%s_3DMC.sdm', sub, run, ...
                     runtype)];
             else
-                mcfname = [subjdir 'images' filesep ...
+                mcfname = [subdir 'images' filesep ...
                     sprintf('ripples_%s_run%d_%s_SCSTBL_3DMC.sdm', sub, run, ...
                     runtype)];
             end
             sdmMC = xff(mcfname);
             sdm.SDMMatrix(:,nfeatures+2:end) = sdmMC.SDMMatrix;
         end
-        sdm.RTCMatrix = sdm.SDMMatrix;
+            sdm.RTCMatrix = sdm.SDMMatrix;
         
-        sdm.SaveAs(sprintf('%s%s_run%d_%s_RippleIndicator%dD%s.sdm',sdmdir, ...
-            sub, run, runtype, nfeatures, fname));
-        clear sdm
+            sdm.SaveAs(sprintf('%s%s_run%d_%s_RippleIndicator%dD%s.sdm', ...
+                sdmdir, sub, run, runtype, nfeatures, fname));
+            clear sdm
+        end
     end
 end
 
@@ -344,27 +384,6 @@ for f=1:nfeatures
     convolved = conv(inmat(:,f), h);
     outmat(1:nvols,f) = convolved(1:nvols);
 end
-
-function [outmat, outnames] = deconvolveHRF(inmat, prednames, nstick)
-if nargin < 3
-    nstick=8;
 end
-nvols = size(inmat,1);
-npreds = length(prednames);
-convmat = cat(1,eye(nstick),zeros(nvols-nstick,nstick));
-outmat = zeros(nvols, npreds*nstick);
-outnames = {};
-for p = 1:npreds
-    for s=1:nstick
-        outnames = cat(2,outnames, [prednames{p} '_D' num2str(s-1)]);
-    end
-    deconved = conv2(inmat(:,p),convmat);
-    outmat(:,p*nstick-nstick+1:p*nstick) = deconved(1:nvols, :); 
-end
-
-
-
-
         
 %------------- END CODE --------------     
-    
